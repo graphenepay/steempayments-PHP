3f60c51f4632037a52cc30549b723f2d28632621
steempayments <H0tmail000**>
2016-07-30 17:47:29 +0200

    configuration file

---
+++ .env.php
@@ -0,0 +1,13 @@
+<?php
+
+/** Configuration file **/
+
+return array(
+
+    'ENABLE_DEBUG'    => true,
+    'STEEMPAY_ACCOUNT' => 'your-account-to-receive-payments',
+
+    'STEEM_RPC_SERVER' => '127.0.0.1',
+    'STEEM_RPC_PORT' => 8090,
+
+);
