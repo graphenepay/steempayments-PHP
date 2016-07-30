2faab63b8f21330a9097a510a011e26126d8f9b8
steempayments <H0tmail000**>
2016-07-30 17:58:20 +0200

    added comments

--- core/.env.php
+++ core/.env.php
@@ -5,9 +5,20 @@
 return array(
 
     'ENABLE_DEBUG'    => true,
+	
+	// The account to receive payments with
     'STEEMPAY_ACCOUNT' => 'your-account-to-receive-payments',
 
+	// Node to plugin
     'STEEM_RPC_SERVER' => '127.0.0.1',
     'STEEM_RPC_PORT' => 8090,
+	
+	// If you like a prefix in your memo, use it here
+	'MEMO_PREFIX' => '',
+	
+	// Exchange settings
+	'PRICE_TICKER' => 'POLONIEX',
+	/* If you want to add a % above the live market data - NOT ADVICED! */
+	'PRICE_CONVERT_PERCENTAGE_EXTRA' => 0,
 
 );
